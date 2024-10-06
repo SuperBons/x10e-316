6775cab7068d8695ad730e3271ae683b8d8a7f9e
ralphsulay <ralphsulay@gmail.com>
2024-10-06 14:53:34 -0700

    load env vars

--- Back-end/scripts/llm_manager.py
+++ Back-end/scripts/llm_manager.py
@@ -2,11 +2,12 @@ from langchain_openai import ChatOpenAI
 from prompt_generator import PromptGenerator
 import getpass
 import os
-
+from dotenv import load_dotenv
 
 #enter OpenAi Api Key
+
 if "OPENAI_API_KEY" not in os.environ:
-    os.environ["OPENAI_API_KEY"] = "Your API KEY"
+    load_dotenv()
 
 
 #Loader class to easily switch out models
